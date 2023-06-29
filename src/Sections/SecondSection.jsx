db11cf170446089d9d55c92a325dfd398c14a1b5
nehemiahcode <codehubby@gmail.com>
2023-06-29 17:01:38 +0100

    error fix2

--- src/Sections/SecondSection.jsx
+++ src/Sections/SecondSection.jsx
@@ -127,7 +127,7 @@ export function Cards() {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {card.heading}
             </h5>
-            <p className="mb-3 font-medium text-lg text-gray-900 dark:text-gray-400">
+            <p className="mb-3 font-medium text-lg text-gray-900 ">
               {card.paragrapg}
             </p>
             <a
